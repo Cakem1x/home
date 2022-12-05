40c13094d170e651aa953329c9372dcff4911dc5
Matthias Holoch <mholoch@gmail.com>
2022-12-05 19:10:09 +0100

    refactor home manager cfg to support multiple machines

---
+++ .config/nixpkgs/trnstr.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./common.nix
+    ];
+
+  home.packages = with pkgs; [
+    # shell basics
+    alacritty
+    # CLI util
+    imagemagick
+    # file viewers
+    evince
+
+    # wm, wm system components, UI utils
+    i3
+    xss-lock
+    networkmanagerapplet
+    pavucontrol
+    playerctl
+
+    # editing
+    ispell
+    libreoffice
+    # latex
+    texlive.combined.scheme-full
+
+    # other apps
+    firefox
+    signal-desktop
+    openscad
+    gnucash
+    aqbanking
+    gnupg
+    pinentry
+    nextcloud-client
+    # unfree
+    spotify
+  ];
+}
