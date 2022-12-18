a0ad2731ca78e869af74b35f5ec1b16afc254ba3
Matthias Holoch <mholoch@gmail.com>
2022-12-18 17:20:20 +0100

    add gpio group for PI, add my user to that grp

--- .config/nix_machines/firefly.nix
+++ .config/nix_machines/firefly.nix
@@ -20,6 +20,16 @@
   # Enable GPU acceleration
   hardware.raspberry-pi."4".fkms-3d.enable = true;
 
+  # Create group for gpio access
+  users.groups.gpio = {};
+  # udev rule for gpio (from https://nixos.wiki/wiki/NixOS_on_ARM/Raspberry_Pi_4)
+  services.udev.extraRules = ''
+    SUBSYSTEM=="bcm2835-gpiomem", KERNEL=="gpiomem", GROUP="gpio",MODE="0660"
+    SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", RUN+="${pkgs.bash}/bin/bash -c 'chown root:gpio  /sys/class/gpio/export /sys/class/gpio/unexport ; chmod 220 /sys/class/gpio/export /sys/class/gpio/unexport'"
+    SUBSYSTEM=="gpio", KERNEL=="gpio*", ACTION=="add",RUN+="${pkgs.bash}/bin/bash -c 'chown root:gpio /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value ; chmod 660 /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value'"
+  '';
+  users.users.cakemix.extraGroups = ["gpio"];
+
   # Home Assistant cfg
   services.home-assistant = {
     enable = true;
