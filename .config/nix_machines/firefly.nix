8c7779d2aeda203aee6f1498b89145ff3f555206
Matthias Holoch <mholoch@gmail.com>
2022-12-23 12:26:42 +0100

    activate raspi 4 hardware features, add user to i2c grp

--- .config/nix_machines/firefly.nix
+++ .config/nix_machines/firefly.nix
@@ -17,6 +17,15 @@
     };
   };
 
+  hardware.raspberry-pi."4" = {
+    fkms-3d.enable = true;
+    audio.enable = true;
+    pwm0.enable = true;
+    i2c0.enable = true;
+    i2c1.enable = true;
+  };
+  users.users.cakemix.extraGroups = ["i2c"];
+
   # Home Assistant cfg
   services.home-assistant = {
     enable = true;
