5685ad02e64af871fd47a2ebf9086d5cd2f86b06
Matthias Holoch <mholoch@gmail.com>
2022-12-28 19:38:44 +0100

    refactor sops section

--- .config/nix_machines/common.nix
+++ .config/nix_machines/common.nix
@@ -20,12 +20,13 @@
     # You can avoid this by adding a string to the full path instead, i.e.
     # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
     defaultSopsFile = ./secrets/secrets.yaml;
-    # This will automatically import SSH keys as age keys
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-    # This is using an age key that is expected to already be in the filesystem
-    age.keyFile = "/var/lib/sops-nix/key.txt";
-    # This will generate a new key if the key specified above does not exist
-    age.generateKey = true;
+    # Auto-generate and use age key based on a machines ssh key.
+    # Mostly used for decrytping on machines where cfg is deployed.
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true;
+    };
   };
 
   networking = {
