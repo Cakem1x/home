66132fb4221928038c65063e910ac7a6a29f4bb3
Matthias Holoch <mholoch@gmail.com>
2024-01-23 10:54:06 +0100

    fix wifi on ice

--- .config/nix_machines/trnstr.nix
+++ .config/nix_machines/trnstr.nix
@@ -71,7 +71,19 @@
 
   services.autorandr.enable = true;
 
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = { # fix for WifiOnICE; Moves docker network IP ranges away from what the Deutsche Bahn wifi uses.
+      bip = "172.39.1.5/24";
+      fixed-cidr = "172.39.1.0/25";
+      default-address-pools =
+      [{
+          base= "172.39.0.0/16";
+          size= 24;
+      }];
+    };
+  };
+
   users.users.cakemix.extraGroups = ["docker" "dialout"];
   # fix steam
   hardware.opengl.driSupport32Bit = true;
